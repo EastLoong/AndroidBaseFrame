185df2db0eaa3dca9784286822457a991f54d3b9
EastLoong <517367549@qq.com>
2018-04-08 23:35:18 +0800

    提交测试

--- app/src/main/java/com/androidbaseframe/MainActivity.kt
+++ app/src/main/java/com/androidbaseframe/MainActivity.kt
@@ -7,6 +7,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //测试
         setContentView(R.layout.activity_main)
     }
 }
